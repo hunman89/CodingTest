998647737a0dc2e20be3feeb40d962e71dcb01bc
SungHun Jung <hunman89@gmail.com>
2022-11-29 07:00:19 +0000

    add. leet 380

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-class FunEvent:
-    def __init__(self, tags, year):
-        self.tags = tags
-        self.year = year
-    
-    def __str__(self):
-        return f"FunEvent(tags={self.tags}, year={self.year})"
+matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
 
-tags = ["google", "ml"]
-year = 2022
-bootcamp = FunEvent(tags, year)
-tags.append("bootcamp")
-year = 2023
-print(bootcamp)
+print(*matches)
+print(list(zip(*matches)))
