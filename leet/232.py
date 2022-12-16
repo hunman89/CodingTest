bcdecce01d37ab784087894f45f0df2a21c35674
SungHun Jung <hunman89@gmail.com>
2022-12-16 01:37:23 +0000

    add. leet 232

--- leet/232.py
+++ leet/232.py
@@ -1,36 +1,31 @@
 class MyQueue:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.input = []
-        self.output = []
+        self.queue = []
+        self.stack = []
 
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
-        self.input.append(x)
+        self.queue.append(x)
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
-        self.peek()
-        return self.output.pop()
+        self.move()
+        return self.stack.pop()
 
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output[-1]
+        self.move()
+        return self.stack[-1]
 
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        return self.input == [] and self.output == []
+        return True if (len(self.queue) is 0) and (len(self.stack) is 0) else False
+
+    def move(self):
+        if len(self.stack) is 0:
+            while len(self.queue) is not 0:
+                self.stack.append(self.queue.pop())
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
