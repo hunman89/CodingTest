3631f13126e80bf70ec5c8a6390f8a956a63d948
SungHun Jung <hunman89@gmail.com>
2023-02-03 01:31:24 +0000

    add. leet 6

--- leet/6.py
+++ leet/6.py
@@ -1,17 +1,38 @@
-def convert(self, s: str, numRows: int) -> str:
-    cycle = numRows*2 -2
-    if cycle == 0:
-        return s
+def convert(self, s: str, numRows: int) -> str:   
+    if numRows == 1: return s
+        
+    rows = ["" for i in range(numRows)]
     
-    strings = []
+    i = 0
+    j = 0
+    direct = True
     
-    for i in range(numRows):
-        strings.append("")
-    
-    for i in range(len(s)):
-        k = i % cycle
-        if k < numRows:
-            strings[k] += s[i]
+    while i < len(s):
+        rows[j] += s[i]
+        i += 1
+        if j == numRows - 1:
+            direct = False
+        if j == 0:
+            direct = True
+        if direct:
+            j += 1
         else:
-            strings[numRows*2 - 2 - k] += s[i]
-    return "".join(strings)
+            j -= 1
+    return "".join(rows)
+
+    # cycle = numRows*2 -2
+    # if cycle == 0:
+    #     return s
+    
+    # strings = []
+    
+    # for i in range(numRows):
+    #     strings.append("")
+    
+    # for i in range(len(s)):
+    #     k = i % cycle
+    #     if k < numRows:
+    #         strings[k] += s[i]
+    #     else:
+    #         strings[numRows*2 - 2 - k] += s[i]
+    # return "".join(strings)
